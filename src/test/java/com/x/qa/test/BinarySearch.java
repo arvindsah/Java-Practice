395a7e88fd5d312e08eb61946bf9d41c9656747d
Arvind Sah <arvindsah005@gmail.com>
2022-02-21 23:20:50 +0530

    liner and binary search

--- src/test/java/com/x/qa/test/BinarySearch.java
+++ src/test/java/com/x/qa/test/BinarySearch.java
@@ -6,25 +6,39 @@ import java.util.Scanner;
 import org.testng.annotations.Test;
 
 public class BinarySearch {
+	
+	/**
+	 * 1. search number using linear search 
+	 * 2. search number using binary search (Using Arrays)
+	 */
 	private final static int LENGTH=5;
 	@Test
 	public void searchNumberUsingArraysBinarySearch() {
 		
 			int[] numberArray= new int[LENGTH];
 			int numberToSearch;
+			int userInputInterger;
 			System.out.println("Enter "+LENGTH+ " numbers into array");
 			Scanner scan= new Scanner(System.in);
-			for(int index:numberArray) {
-				System.out.println("enter number to add into array");
-				numberArray[index]= scan.nextInt();
+			for(int index=0;index<LENGTH; index++) {
+				//System.out.println("enter number to add into array");
+				//numberArray[index]= scan.nextInt();	
+				do {
+					System.out.println("Enter "+(index+1)+"th number to add into array");
+					userInputInterger= scan.nextInt();	
+				}while(searchNumberManully(numberArray, userInputInterger));
+				
+				numberArray[index]=userInputInterger;
+				
 			}
 			
-			System.out.println("enter the number to search in array" );
+			System.out.println("Enter the number to search in array" );
 			numberToSearch= scan.nextInt();
 			boolean isNumberExistInArray=searchNumber(numberArray, numberToSearch);
 			System.out.println("is number already present in the array - " +isNumberExistInArray);
 		
 	}
+	
 	private boolean searchNumber(int[] numberArray, int numberToSearch) {
 		Arrays.sort(numberArray);
 		 int searchResult=Arrays.binarySearch(numberArray, numberToSearch);
@@ -33,8 +47,15 @@ public class BinarySearch {
 			 return true;
 		 }
 		 return false;
-		//Arrays.sort(numberArray);
-		
 	}
-
+	
+	private boolean searchNumberManully (int[] numberArray, int numberToSearch) {
+		for(int value:numberArray) {
+			if(value==numberToSearch) {
+				System.out.println("Number found inside the numberArray");
+				return true;
+			}
+		}
+		return false;
+	}
 }
