24ea9b60ab1d1cd954dbe48178b5382b5248e651
Arvind Sah <arvindsah005@gmail.com>
2022-03-04 00:22:05 +0530

    longest prefix 2

--- src/test/java/com/x/qa/test/leetcode/CommonPrefix.java
+++ src/test/java/com/x/qa/test/leetcode/CommonPrefix.java
@@ -1,9 +1,15 @@
 package com.x.qa.test.leetcode;
 
+import org.testng.annotations.Test;
+
 public class CommonPrefix {
 	
-	    public String longestCommonPrefix() {
-	    	String[] strs= new String[] {"flower","flow","flight"};
+	public static void main(String[] args) {
+		longestCommonPrefix();
+	}
+		@Test
+	     static String longestCommonPrefix() {
+	    	String[] strs= new String[] {"cir","car"};
 	        /*Map <Integer, Integer> map=new Hashmap<>();
 	        for(int i=0;i<strs.length;i++){
 	            map.put(strs[i],strs[i].length());
@@ -15,15 +21,14 @@ public class CommonPrefix {
 	        int min=strs[0].length();
 	        String lowestStr="";
 	         for(int i=0;i<strs.length;i++){
-	         if(strs[i].length()<min){
+	         if(strs[i].length()<=min){
 	             lowestStr=strs[i];
 	             min=strs[i].length();
 	         }
 	         }
 	        System.out.println(lowestStr);
-	        String predicate="";
 	        for(int i=0;i<strs.length;i++){
-	            System.out.println("running loop for i " + i);
+	            System.out.println("running loop for i " + i + " - " + strs[i] );
 	            for(int j=0;j<lowestStr.length();j++){
 	            if(strs[i].charAt(j)== lowestStr.charAt(j)){
 	                continue;
@@ -36,6 +41,6 @@ public class CommonPrefix {
 	             System.out.println(strs.length);
 	         }             
 	        }
-	       return predicate; 
+	       return lowestStr; 
 	    }
 }
